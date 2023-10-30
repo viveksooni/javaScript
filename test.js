21bc0dc90d163d5aaaf4d4c345ffc8088bcd42c8
vivek <sonivivek346@gmail.com>
2023-10-30 22:39:25 +0530

    data types

--- test.js
+++ test.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const accId = 12323423;
+var name = "Vivek";
+let first = "second";
+num = 234;
+let a;
+// we prefer using "let" and avoid using "var" because of block scope and functional scope;
+
+console.table([accId,name,first,num,a]);
+
+/* data types in js
+    number ( 2^53 range )
+    bigInt
+    string 
+    boolean
+    null // is a object(debateable)
+    undefined
+    symbol
+_________________
+
+    objects
+*/
+
+console.log(typeof null);
+console.log(typeof num);
+console.log(typeof undefined);
